4092e0adc6f77b0794bb862b5f1590b7c1c45343
Geoff Schmit <gcschmit@gmail.com>
2023-04-17 15:10:25 -0500

    add sample binary search solution

--- BinarySearcher.java
+++ BinarySearcher.java
@@ -15,7 +15,38 @@ public class BinarySearcher
    */
    public static int search(int[] a, int low, int high, int value)
    {  
-      return 0;
+        // terminating condition 0 or 1 element to search
+        if(low >= high)
+        {
+            if(a[low] == value)
+            {
+                return low;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        
+        // take a small step towards a solution by finding the midpoint
+        //   of the specified range
+        int midIndex = (high + low) / 2;
+        
+        if(value < a[midIndex])
+        {
+            // value must be in the lower half of the range
+            return search(a, low, midIndex - 1, value);
+        }
+        else if(value > a[midIndex])
+        {
+            // value must be in the upper half of the range
+            return search(a, midIndex + 1, high, value);
+        }
+        else // a[midIndex] == value
+        {
+            // we found the value!
+            return midIndex;
+        }
    }
 }
 
